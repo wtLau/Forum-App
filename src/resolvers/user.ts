6c49f3b0f4e6f8cb6d3e5adfb6dbbefc33975ac6
Brian Lau <lauwangtatbrian@gmail.com>
2020-12-29 20:45:54 -0800

    MikroORM, Apollo Server, Express TypeGraphQL Crud set-up

---
+++ src/resolvers/user.ts
@@ -0,0 +1,79 @@
+import { MyContext } from "src/types";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import argon2 from 'argon2'
+import { User } from "../entities/User";
+
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string
+  @Field()
+  password: string
+}
+
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string
+  @Field()
+  message: string
+}
+
+@ObjectType() 
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[]
+
+  @Field(() => User, { nullable: true})
+  user?: User
+}
+
+
+@Resolver()
+export class UserResolver {
+  //REGISTER
+  @Mutation(() => User)
+  async register (
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() {em}: MyContext
+    ) {
+      const hashedPassword = await argon2.hash(options.password)
+      const user = em.create(User, {
+        username: options.username,
+        password: hashedPassword
+      })
+      await em.persistAndFlush(user)
+    return user
+  }  
+
+  //LOGIN
+  @Mutation(() => UserResponse)
+  async login (
+    @Arg('options') options: UsernamePasswordInput,
+    @Ctx() {em}: MyContext
+    ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username})
+    if (!user) {
+      return {
+        errors: [{
+          field: "username",
+          message: 'That username doesnt exist'
+        }]
+      }
+    }
+    const valid = await argon2.verify(user.password, options.password)
+    if(!valid) {
+      return {
+        errors: [{
+          field: "password",
+          message: 'Incorrect password'
+        }]
+      }
+    }
+
+    return {
+      user,
+    }
+  }  
+}
