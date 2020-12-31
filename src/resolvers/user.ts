5e87b7ecaa626f74b170f1f6bca619b43e19ded2
Brian Lau <lauwangtatbrian@gmail.com>
2020-12-30 19:51:04 -0800

    add session features

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,5 @@
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Query, Resolver } from "type-graphql";
 import argon2 from 'argon2'
 import { User } from "../entities/User";
 
@@ -20,12 +20,12 @@ class FieldError {
   message: string
 }
 
-@ObjectType() 
+@ObjectType()
 class UserResponse {
   @Field(() => [FieldError], { nullable: true })
   errors?: FieldError[]
 
-  @Field(() => User, { nullable: true})
+  @Field(() => User, { nullable: true })
   user?: User
 }
 
@@ -33,48 +33,64 @@ class UserResponse {
 @Resolver()
 export class UserResolver {
   //REGISTER
+  @Query(() => User, { nullable: true })
+  async me(
+    @Ctx() { req, em }: MyContext
+  ) {
+
+    // Not Logged In Condition
+    if (!req.session.userId) {
+      return null
+    }
+
+    // Give User if its logged in
+    const user = await em.findOne(User, { id: req.session.userId })
+    return user
+  }
+
+
   @Mutation(() => UserResponse)
-  async register (
+  async register(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() {em}: MyContext
-    ): Promise<UserResponse> {
+    @Ctx() { em }: MyContext
+  ): Promise<UserResponse> {
 
-      if(options.username.length <= 2 ){
-        errors: [{
-          field: 'username',
-          message: 'length must be greater than 2'
-        }]
-      }
-      if(options.password.length <= 2 ){
-        errors: [{
-          field: 'password',
-          message: 'length must be greater than 2'
-        }]
-      }
+    if (options.username.length <= 2) {
+      errors: [{
+        field: 'username',
+        message: 'length must be greater than 2'
+      }]
+    }
+    if (options.password.length <= 2) {
+      errors: [{
+        field: 'password',
+        message: 'length must be greater than 2'
+      }]
+    }
 
-      const hashedPassword = await argon2.hash(options.password)
+    const hashedPassword = await argon2.hash(options.password)
 
-      const user = em.create(User, {
-        username: options.username,
-        password: hashedPassword
-      })
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword
+    })
 
-      try {
-        await em.persistAndFlush(user)
+    try {
+      await em.persistAndFlush(user)
 
-      } catch(err) {
-        console.log('Message: ', err.message)
-      }
-    return {user}
-  }  
+    } catch (err) {
+      console.log('Message: ', err.message)
+    }
+    return { user }
+  }
 
-  //LOGIN
+  //LOGIN RESOLVER
   @Mutation(() => UserResponse)
-  async login (
+  async login(
     @Arg('options') options: UsernamePasswordInput,
-    @Ctx() {em}: MyContext
-    ): Promise<UserResponse> {
-    const user = await em.findOne(User, { username: options.username})
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    const user = await em.findOne(User, { username: options.username })
     if (!user) {
       return {
         errors: [{
@@ -84,7 +100,7 @@ export class UserResolver {
       }
     }
     const valid = await argon2.verify(user.password, options.password)
-    if(!valid) {
+    if (!valid) {
       return {
         errors: [{
           field: "password",
@@ -93,8 +109,13 @@ export class UserResolver {
       }
     }
 
+    // Store user id session
+    // this will set a cookie on the user
+    // keep them logged in
+    req.session.userId = user.id
+
     return {
       user,
     }
-  }  
+  }
 }
